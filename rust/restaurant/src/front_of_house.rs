820d2d91278aa0a871264f04083b9b570f7f1858
SamyadeepSaha <samyadeep98@gmail.com>
2024-02-10 10:37:20 +0530

    Add rust example for crates and modules

---
+++ rust/restaurant/src/front_of_house.rs
@@ -0,0 +1,13 @@
+// Definition of front_of_house
+
+// Declaration of hosting
+pub mod hosting;
+
+// Declaration and definition of serving
+mod serving {
+    fn take_order() {}
+
+    fn serve_order() {}
+
+    fn take_payment() {}
+}
---
+++ rust/restaurant/src/front_of_house/hosting.rs
@@ -0,0 +1,4 @@
+// Definition of hosting
+pub fn add_to_waitlist() {}
+
+fn seat_at_table() {}
