c4b7d1393b927226a31735eb8aca8f9d0942ad94
SamyadeepSaha <samyadeep98@gmail.com>
2024-02-17 23:09:54 +0530

    Add some example of hash map in rust
    Clean binaries in hash map crate

---
+++ rust/hash_map/src/main.rs
@@ -0,0 +1,24 @@
+use std::collections::HashMap;
+
+fn main() {
+    let mut scores = HashMap::new();
+
+    scores.insert(String::from("Blue"), 10);
+    scores.insert(String::from("Yellow"), 50);
+
+    let team_name = String::from("Blue");
+    let score = scores.get(&team_name).copied().unwrap_or(0);
+    println!("{score}");
+
+    for(key, value) in &scores {
+        println!("{key}: {value}");
+    }
+
+    /* Hash maps and Ownership */
+    let field_name = String::from("Favorite color");
+    let field_value = String::from("Blue");
+
+    let mut map = HashMap::new();
+    map.insert(field_name, field_value);
+    /* field_name and field_value are invalid at this point */
+}
