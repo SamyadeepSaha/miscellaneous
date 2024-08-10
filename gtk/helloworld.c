481cade175b0fca6f777411d3741f71a52979b52
SamyadeepSaha <samyadeep98@gmail.com>
2024-08-10 12:52:22 +0530

    Add an example of gtk 2

---
+++ gtk/helloworld.c
@@ -0,0 +1,21 @@
+/* This program is a example of gtk 2
+ * Compile command:
+ * gcc -Wall `pkg-config --cflags gtk+-2.0` helloworld.c `pkg-config --libs gtk+-2.0` -o hello 
+ */
+#include<gtk/gtk.h>
+
+int main(int argc, char *argv[]) {
+    GtkWidget *window;
+
+    /* Initialize GTK+ and all of its supportin libraries */
+    gtk_init(&argc, &argv);
+
+    /* Create a new window, give it a title and display it to the user */
+    window = gtk_window_new(GTK_WINDOW_TOPLEVEL);
+    gtk_window_set_title(GTK_WINDOW(window), "Hello World");
+    gtk_widget_show(window);
+
+    /* Hand control over to the main loop */
+    gtk_main();
+    return 0;
+}
