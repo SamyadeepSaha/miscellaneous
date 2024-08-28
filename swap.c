500c36a95619d7f2f5a13ebe0d30f3ceefa96b47
SamyadeepSaha <samyadeep98@gmail.com>
2024-08-28 07:35:13 +0530

    Add function for swap by referece

--- swap.c
+++ swap.c
@@ -1,9 +1,16 @@
 /* Following code swap two variable */
 #define SWAP(p,q)  p += q; q = p -q; p -= q; 
+void swap_func(int *a, int *b) {
+    int temp;
+    temp = *a;
+    *a = *b;
+    *b = temp;
+}
 #include<stdio.h>
 int main() {
     char x = 'a';
     char y = 'b';
+    int p = 3, q = 4;
     /*
     x += y;
     y = x - y;
@@ -11,5 +18,7 @@ int main() {
     */
     SWAP(x,y);
     printf("x: %c, y: %c\n", x, y);
+    swap_func(&p, &q);
+    printf("p: %d, q: %d\n", p, q);
     return 0;
 }
